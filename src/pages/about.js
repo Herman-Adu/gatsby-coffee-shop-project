20cea7c45131c90dc15dc982c4e6f51c82d81a8e
Herman Adu <herman.adu@hotmail.com>
2020-08-10 15:05:30 +0100

    added info component to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import Info from "../components/Home/Info"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
